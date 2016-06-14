c0176b568c8a88e56c4f29bf051747f88ac9c6b3
sherlock31 <balrajholmes@gmail.com>
2016-06-15 00:10:06 +0530

    ocr for a single file working

--- pdf2image.py
+++ pdf2image.py
@@ -1,13 +1,15 @@
+from wand.image import Image
+from wand.color import Color
+	
 def pdf2image(filepath):
 	"This function accepts a pdf file, it converts into a .jpg image using wand module"
 	
-	from wand.image import Image
-	from wand.color import Color
-	#from PIL import Image
+	image_path = filepath.replace('.pdf','.jpg')				
 	
 	with Image(filename = filepath, resolution=200) as img:
 		img.format = 'jpeg'
 		img.compression_quality = 99
-		img.save(filename='winter.jpg')
+		img.save(filename = image_path)
+		print "PDF has been converted into image"
 		
 		
