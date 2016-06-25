f7985b3edc0c075e0390cd5cd73d513a678aa849
sherlock31 <balrajholmes@gmail.com>
2016-06-25 15:43:50 +0530

    Not much success in IP, will edit the code to original stage to give the code to Ravi

--- folder_run.py
+++ folder_run.py
@@ -1,8 +1,17 @@
 import os
-from ocr import ocr
+#from ocr import ocr
+#from pdf2image import pdf2image
+#from calc_angle import calc_angle
+#from rotate_image import rotate_image
+#from denoise_kaggle import denoise 
+#from rgb2gray import rgb2gray
+from image2txt import image2txt
 
 def folder_run(folder_path):
 	"This function accepts path to a folder as input and runs the OCR function on all the files"
 	for file_name in os.listdir(folder_path):
-		ocr(folder_path + "/" + file_name)
-		
+		#angle_rot = calc_angle(folder_path + "/" + file_name)
+		#rotate_image(folder_path + "/" + file_name, angle_rot)
+		#denoise(folder_path + "/" + file_name)
+		#rgb2gray(folder_path + "/" + file_name)
+		image2txt(folder_path + "/" + file_name)
