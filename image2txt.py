2504ccbba1330691de9ab13102ca01ec323b8eb9
sherlock31 <balrajholmes@gmail.com>
2016-06-30 14:33:16 +0530

    Word_Counter implemented, med_data.txt added

--- image2txt.py
+++ image2txt.py
@@ -4,8 +4,8 @@ from PIL import Image
 def image2txt(imagepath):
 	"This function accepts path to a jpg image file , it does OCR on it using Google's Tesseract engine and writes the extracted data in a .txt file"
 	
-	text_temp = imagepath.replace('.jpg', '.txt')							#Name of the text file will be same as the name of the image
-	file_output = open(text_temp, 'w')										#Opening a text file for writing
+	text_out = imagepath.replace('.jpg', '.txt')							#Name of the text file will be same as the name of the image
+	file_output = open(text_out, 'w')										#Opening a text file for writing
 	file_output.write(pytesseract.image_to_string(Image.open(imagepath)))	#Writing the extracted text into the opened text file
 	
 	print "image to txt conversion is complete"
