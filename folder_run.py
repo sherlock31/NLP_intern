a1cc8398b021ffb48e94e107cd480e78eaeb4d9a
sherlock31 <balrajholmes@gmail.com>
2016-07-05 18:03:27 +0530

    code working for word documents

--- folder_run.py
+++ folder_run.py
@@ -1,19 +1,8 @@
 import os
 from ocr import ocr
-#from pdf2image import pdf2image
-#from calc_angle import calc_angle
-#from rotate_image import rotate_image
-#from denoise_kaggle import denoise 
-#from rgb2gray import rgb2gray
-from image2txt import image2txt
 
 def folder_run(folder_path):
 	"This function accepts path to a folder as input and runs the OCR function on all the files"
 	
 	for file_name in os.listdir(folder_path):
-		#angle_rot = calc_angle(folder_path + "/" + file_name)
-		#rotate_image(folder_path + "/" + file_name, angle_rot)
-		#denoise(folder_path + "/" + file_name)
-		#rgb2gray(folder_path + "/" + file_name)
-		#image2txt(folder_path + "/" + file_name)
 		ocr(folder_path + "/" + file_name)
